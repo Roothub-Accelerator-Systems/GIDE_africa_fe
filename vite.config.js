a9b6f933335025a7d2901b58849008ef3208ec84
Israel-Iraoya <israeliraoya7@gmail.com>
2025-06-23 13:25:03 +0100

    trying to fix the port issue

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const BASE_PORT = import.meta.env.BASE_PORT;
-
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: parseInt(BASE_PORT) || 8080, // use Render's provided port or fallback
-    host: '0.0.0.0', // must be 0.0.0.0 for Render to detect
-  },
+    port: 5173,
+    host: '0.0.0.0',
+  }
 })
